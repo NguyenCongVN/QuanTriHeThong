2447932be8d7149af3be609a4d602eb610501891
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-09-05 21:53:51 +0700

    Update phan quyen

---
+++ DXApplication1/Utilizes/ComboBoxItemPhanQuyen.cs
@@ -0,0 +1,19 @@
+﻿using DXApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication1.Utilizes
+{
+    public class ComboBoxItemPhanQuyen
+    {
+        public Chucvu ChucVu { get; set; }
+
+        public override string ToString()
+        {
+            return ChucVu.TenChucVu;
+        }
+    }
+}
