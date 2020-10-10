6ce8af64c2807c9dca9c7b263fa9731a82a23556
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-10-10 16:32:14 +0700

    Revert "Revert "Revert "Revert "Mo complete""""
    This reverts commit fe3bc4602c6dcf400d813dd6fb0b7d188c4c5c98.

--- DXApplication1/Models/DoiTuong.cs
+++ DXApplication1/Models/DoiTuong.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DXApplication1.Views;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,11 @@ namespace DXApplication1.Models
     class DoiTuong : Form
     {
         PictureBox picture;
+        private ContextMenuStrip contextMenuStrip1;
+        private System.ComponentModel.IContainer components;
+        private ToolStripMenuItem thôngTinToolStripMenuItem;
+        private ToolStripMenuItem chỉnhSửaToolStripMenuItem;
+        private ToolStripMenuItem xóaToolStripMenuItem;
         string detail;
         public enum DoiTuongFields
         {
@@ -17,24 +24,108 @@ namespace DXApplication1.Models
             Detail
         }
         public PictureBox Picture { get => picture; set => picture = value; }
-        public string Detail { get => detail; set => detail = value; }
+        public  string Detail { get => detail; set => detail = value; }
 
-        public DoiTuong()
+
+        public DoiTuong(ContextMenuStrip context)
         {
+            InitializeComponent();
             picture = new PictureBox();
             picture.BackColor = System.Drawing.Color.Transparent;
             picture.Size = new System.Drawing.Size(13, 20);
             picture.SizeMode = PictureBoxSizeMode.AutoSize;
-            picture.Click += new System.EventHandler(this.picClick);
+            picture.ContextMenuStrip = contextMenuStrip1;
         }
         public DoiTuong(PictureBox _pic, string _detail)
         {
             this.picture = _pic;
             this.detail = _detail;
         }
-        public void picClick(object sender, EventArgs e)
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.thôngTinToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chỉnhSửaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xóaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thôngTinToolStripMenuItem,
+            this.chỉnhSửaToolStripMenuItem,
+            this.xóaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(215, 110);
+            // 
+            // thôngTinToolStripMenuItem
+            // 
+            this.thôngTinToolStripMenuItem.Image = global::DXApplication1.Properties.Resources.iconPro;
+            this.thôngTinToolStripMenuItem.Name = "thôngTinToolStripMenuItem";
+            this.thôngTinToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.thôngTinToolStripMenuItem.Text = "Thông tin";
+            this.thôngTinToolStripMenuItem.Click += new System.EventHandler(this.thôngTinToolStripMenuItem_Click);
+            // 
+            // chỉnhSửaToolStripMenuItem
+            // 
+            this.chỉnhSửaToolStripMenuItem.Image = global::DXApplication1.Properties.Resources.iconChinhsua;
+            this.chỉnhSửaToolStripMenuItem.Name = "chỉnhSửaToolStripMenuItem";
+            this.chỉnhSửaToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.chỉnhSửaToolStripMenuItem.Text = "Chỉnh sửa";
+            this.chỉnhSửaToolStripMenuItem.Click += new System.EventHandler(this.chỉnhSửaToolStripMenuItem_Click);
+            // 
+            // xóaToolStripMenuItem
+            // 
+            this.xóaToolStripMenuItem.Image = global::DXApplication1.Properties.Resources.iconDelete;
+            this.xóaToolStripMenuItem.Name = "xóaToolStripMenuItem";
+            this.xóaToolStripMenuItem.Size = new System.Drawing.Size(146, 26);
+            this.xóaToolStripMenuItem.Text = "Xóa";
+            this.xóaToolStripMenuItem.Click += new System.EventHandler(this.xóaToolStripMenuItem_Click);
+            // 
+            // DoiTuong
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "DoiTuong";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void thôngTinToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show(detail);
         }
+
+        private void xóaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            picture.Dispose();
+
+        }
+
+        private void chỉnhSửaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChinhSuaTT cs = new ChinhSuaTT();
+
+            
+            cs.Show();
+            cs.Location = new Point(picture.Location.X + cs.Width + 50, picture.Location.Y + cs.Height);
+            cs.button1.Click += (ss, ee) =>
+            {
+
+                if (cs.textBox1.Text != "")
+                {
+                    detail = cs.textBox1.Text;
+                    MessageBox.Show("Thành công!");
+                    cs.Close();
+                }
+                else
+                    MessageBox.Show("Thông tin trống", "???");
+            };
+        }
+        
     }
 }
